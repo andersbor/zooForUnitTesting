47f13478f110d17b30af2662ef5d68e76b5ec9d2
andersbor <anbo@easj.dk>
2017-08-31 12:33:57 +0200

    first commit

---
+++ UnitTestProject1/ElephantTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using zoo;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ElephantTest
+    {
+        private Elephant _elephant;
+
+        [TestInitialize]
+        public void SetupElephant()
+        {
+             _elephant = new Elephant();
+        }
+
+        [TestMethod]
+        public void TestWeight()
+        {
+            _elephant.Weight = 100;
+            Assert.AreEqual(100, _elephant.Weight);
+
+            _elephant.Weight = 101;
+            Assert.AreEqual(101, _elephant.Weight);
+
+            try
+            {
+                _elephant.Weight = 99;
+                Assert.Fail();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestWeightIllegal()
+        {
+            _elephant.Weight = 99;
+        }
+    }
+}
---
+++ zoo/Elephant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace zoo
+{
+    /// <summary>
+    /// Class describing an elephant in a zoo
+    /// </summary>
+    public class Elephant
+    {
+        private int _weight;
+
+        /// <summary>
+        /// The weight of the elephant
+        /// Measured in kilograms
+        /// Pre-condition: <code>Weight >= 100</code>
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">if weight &lt; 100</exception>
+        public int Weight
+        {
+            get { return _weight; }
+            set
+            {
+                if (value < 100) throw new ArgumentOutOfRangeException("weight", value, "Weight must be at least 100 kg");
+                _weight = value;
+                // HACK to be changed ...
+            }
+        }
+
+        // TODO add Height property
+    }
+}
